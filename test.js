2cda2a984350483ab325a8ccbf1fbce3a793bfce
Dan Richert <dan.richert@gmail.com>
2017-07-17 00:16:25 -0400

    wip test script, training set prep

--- test.js
+++ test.js
@@ -1,10 +1,63 @@
+var fs = require("fs")
 var _ = require("underscore");
+//
+//var a = ["aaa", "zzzzzzzzz", "aaaa", "aaaaa"];
+//
+//var longest = _.reduce(a, (mem, v) => {
+//  //mem = "" + mem
+//  return mem.length > v.length ? mem : v
+//})
+//
+//console.log(longest)
 
-var a = ["aaa", "zzzzzzzzz", "aaaa", "aaaaa"];
+// n-gram length
+const N = 3
 
-var longest = _.reduce(a, (mem, v) => {
-  //mem = "" + mem
-  return mem.length > v.length ? mem : v
+var binVal = chars => {
+  let mask = [0, 0, 0, 0, 0, 0, 0]
+  let out  = [];
+
+  chars.split("").forEach(chr => {
+    let buffer = [];
+    let bits = chr.toString(2).split("")
+
+    mask.forEach((v, ndx) => {
+      buffer[ndx] = bits[ndx]
+    })
+
+    out.concat(buffer.map(b => {
+      return typeof(b) === "undefined" ? "0" : b
+    }))
+  })
+
+  return out
+}
+
+var text = fs
+  .readFileSync("share/heart-of-darkness.txt")
+  .toString()
+
+var trainingSet = text
+  .split("")
+  .map((c, ndx) => {
+    return {
+      input: binVal(c),
+      output: binVal(text.slice(ndx + 1, ndx  + 1 + N)
+    }
+  })
+
+var len = text.length
+
+var trainingSet = (input, output) => {
+  if(typeof(this.set) === "undefined") this.set = []
+
+  if(arguments.length > 0) {
+    this.set.push({ input: input, output: output })
+  }
+
+  return this.set
+}
+
+_(len).times(n => {
+  console.log(text[n % len])
 })
-
-console.log(longest)
